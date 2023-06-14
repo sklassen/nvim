fc44dabac3ea68ec87f4f7340c2987384984ed5d
Simon Klassen <6997477+sklassen@users.noreply.github.com>
2023-06-14 13:30:30 +0800

    file tree

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ git clone git@github.com:sklassen/nvim.git nvim
 # packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# In vi
+:PackerInstall
+
+
+:LspInstall
--- init.lua
+++ init.lua
@@ -13,15 +13,25 @@ require('packer').startup(function(use)
 
   -- Tabs
   use 'bagrat/vim-buffet'
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
 
   -- LSP
   use {
     'VonHeikemen/lsp-zero.nvim',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
       {'williamboman/mason.nvim'},
       {'williamboman/mason-lspconfig.nvim'},
+      {'neovim/nvim-lspconfig'},
 
       -- Autocompletion
       {'hrsh7th/nvim-cmp'},
@@ -39,12 +49,75 @@ require('packer').startup(function(use)
 
 end)
 
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
 vim.opt.signcolumn = 'yes'
 vim.opt.termguicolors = true
 pcall(vim.cmd, 'colorscheme onedark')
 
--- local lsp = require('lsp-zero')
+local lsp = require('lsp-zero')
 
---lsp.preset('recommended')
---lsp.setup()
+lsp.preset('recommended')
+lsp.setup()
 
+
+-- Key Mappings
+local keymap = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+
+-- Switch buffer
+keymap("n", "<C-Left>", ":bprevious<CR>", default_opts)
+keymap("n", "<C-Right>", ":bnext<CR>", default_opts)
+keymap("n", "<C-Up>", ":NvimTreeToggle<CR>", default_opts)
